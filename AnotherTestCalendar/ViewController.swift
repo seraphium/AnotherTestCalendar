4985b787845a8aad8cd7871a66b2c77b6b757d26
Jackie Zhang <jackiezhang@JackiedeMac.local>
2016-06-21 17:26:19 +0800

    first commit on test project Calendar

--- AnotherTestCalendar/ViewController.swift
+++ AnotherTestCalendar/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
 
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.calendarView.dataSource = self
+        self.calendarView.delegate = self
+        self.calendarView.registerCellViewXib(fileName: "CellView")
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func configureCalendar(calendar: JTAppleCalendarView) -> (startDate: NSDate, endDate: NSDate, numberOfRows: Int, calendar: NSCalendar) {
+        // You can set your date using NSDate() or NSDateFormatter. Your choice.
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        
+        let firstDate = formatter.dateFromString("2016 01 05")
+        let secondDate = NSDate()
+        let numberOfRows = 6
+        let aCalendar = NSCalendar.currentCalendar() // Properly configure your calendar to your time zone here
+        
+        return (startDate: firstDate!, endDate: secondDate, numberOfRows: numberOfRows, calendar: aCalendar)
+    }
+    
+    func calendar(calendar: JTAppleCalendarView, isAboutToDisplayCell cell: JTAppleDayCellView, date: NSDate, cellState: CellState) {
+        (cell as! CellView).setupCellBeforeDisplay(cellState, date: date)
+    }
+    
 }
 
